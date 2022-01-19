e2c104ec0dfc386c9362ad477298afc3999fb915
yawen <S2004001@samebest.com.tw>
2022-01-19 18:19:23 +0800

    修改 js

--- Week2/js/all.js
+++ Week2/js/all.js
@@ -5,8 +5,8 @@ createApp ({
     data(){
         return {
             user: {
-                username: 'example@test.com',
-                password: 'example',
+                username: 'yuwen7005@gmail.com',
+                password: '',
             }
         }
     },
@@ -15,9 +15,13 @@ createApp ({
             const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
             axios.post(api, this.user).then((response) => {
                 console.log(response.data);
+                const {token, expired} = response.data;
+                //待了解
+                document.cookie=`token=${token}; expired=${new Date(expired)}; path=/`
                 // window.location = 'products.html';
             }).catch((err) => {
                 console.log(err.data.message);
+                alert(err.data.message);
             });
         },
     },
