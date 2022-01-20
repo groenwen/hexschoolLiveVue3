ca60ab8deed4e16db0a4f30cdb063d0b9a3e6624
yawen <S2004001@samebest.com.tw>
2022-01-20 16:25:52 +0800

    js 修改

--- Week2/js/all.js
+++ Week2/js/all.js
@@ -13,20 +13,20 @@ createApp ({
     methods: {
         login() {
             const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
-            
             axios.post(api, this.user)
             .then((res) => {
-                //登入成功
-                
+                // 登入成功
                 // 取得 登入資料 並存入 cookie
-                console.log(res.data);
-                const {expired, token} = res.data;
-                //path 指定哪些路徑可以存取 cookie
+                // cookie - path 指定哪些路徑可以存取 cookie
+                // cookie - expires 設定 cookie 的使用期限
+                const {token, expired} = res.data;
                 document.cookie=`groenToken=${token};expires=${new Date(expired)};path=/`;
+                
                 //轉至 產品頁面
                 window.location = 'products.html';
             }).catch((err) => {
-                //登入失敗 跳出錯誤訊息
+                //登入失敗
+                //顯示 錯誤訊息
                 alert(err.data.message);
             });
         },
--- Week2/js/products.js
+++ Week2/js/products.js
@@ -12,39 +12,46 @@ createApp ({
     methods: {
         //確認使用者
         checkAdmin(){
+            //驗證 API
             const url = `${this.apiUrl}/api/user/check`;
             axios.post(url)
                 .then((res) => {
+                    //驗證成功
+                    //取得 產品資料
                     this.getProducts();
                 })
                 .catch((err) => {
-                    console.log(err);
+                    //驗證錯誤 回到登入頁面
+                    alert(err.data.message);
+                    window.location = 'index.html';
                 });
         },
         getProducts(){ 
-            console.log('hello')
+            //管理控制台- 產品 API
             const url = `${this.apiUrl}/api/${this.api_path}/admin/products`;
             axios.get(url)
                 .then((res) => {
+                    // 取得 Products 存入 this.products 
                     this.products = res.data.products;
                     console.log(res.data.products);
                 })
-                .catch((data) => {
-
+                .catch((err) => {
+                    alert(err.data.message);
             });
         },
         productItem(){
-            
+
         }
     },
     mounted() {
         //取出 token
+        //再了解
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)groenToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        console.log(token);
+        //全域 axios 授權是否等於 token
         axios.defaults.headers.common.Authorization = token;
 
-        console.log(document.cookie);
-
-        this.checkAdmin()
+        this.checkAdmin();
     }
 
 }).mount('#app');
