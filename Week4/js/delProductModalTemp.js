85a1a1d5cc39d8d59a6c18b8971724e07f9495e3
yawen <S2004001@samebest.com.tw>
2022-02-18 18:06:46 +0800

    edit

---
+++ Week4/js/delProductModalTemp.js
@@ -0,0 +1,37 @@
+export default {
+    data(){
+        return {
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            api_path: 'groen'
+        }
+    },
+    template: '#delProductModalTemp',
+    props: ['isNew', 'productItem'],
+    methods: {
+        // 刪除產品
+        delProduct(){
+            const url = `${this.apiUrl}/api/${this.api_path}/admin/product/${this.productItem.id}`;
+
+            axios.delete(url, this.productItem.id)
+            .then((res) => {
+                alert(res.data.message);
+                //關閉 modal
+                delProductModal.hide();
+                //更新 產品列表
+                this.$emit('update');
+            })
+            .catch((err) => {
+                alert(err.data.message)
+            });
+        },
+    },
+    mounted(){
+        // bootstrap modal 建立
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            backdrop: 'static',
+            keyboard: false,
+
+        });
+        
+    }
+}
--- Week4/js/products.js
+++ Week4/js/products.js
@@ -1,4 +1,7 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+import pagination from '../js/pagination.js';
+import productModalTemp from '../js/productModalTemp.js';
+import delProductModalTemp from '../js/delProductModalTemp.js';
 // 登入 - (API) 進入產品頁面 (/v2/admin/signin)
 // 產品頁面 - 驗證 (/v2/api/user/check)
 // 產品頁面 - 取得產品資料 顯示於頁面 (/v2/api/groen/admin/products/all)
@@ -9,84 +12,11 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 
 // 建立 元件 使用區域註冊
 
+// 範例中為何在元件中 還要建立 openModal & hideModal 
+
 
 let productModal = null;
 let delProductModal = null;
-
-
-const pagination = {
-    template: '#pagination',
-    props: ['pages'],
-    methods:{
-        emitPages(item){
-            this.$emit('emit-pages', item)
-        }
-    }
-}
-const productModalTemp = {
-    template: '#productModalTemp',
-    props: ['isNew', 'productItem'],
-    methods: {
-        emitUpdateProduct(){
-            this.$emit('emit-update-product');
-        },
-        // 多圖-新增圖片
-        createImages() {
-            this.productItem.imagesUrl = [];
-            this.productItem.imagesUrl.push('');
-        },
-        //多圖-刪除 單一圖片
-        delImage(key) {
-            this.productItem.imagesUrl.splice(key, 1);
-        },
-    },
-    mounted(){
-        // bootstrap modal 建立
-        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
-            backdrop: 'static',
-            keyboard: false,
-            
-        });
-    }
-}
-
-const delProductModalTemp = {
-    data(){
-        return {
-            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
-            api_path: 'groen'
-        }
-    },
-    template: '#delProductModalTemp',
-    props: ['isNew', 'productItem'],
-    methods: {
-        // 刪除產品
-        delProduct(){
-            const url = `${this.apiUrl}/api/${this.api_path}/admin/product/${this.productItem.id}`;
-
-            axios.delete(url, this.productItem.id)
-            .then((res) => {
-                alert(res.data.message);
-                //關閉 modal
-                delProductModal.hide();
-                //更新 產品列表
-                this.$emit('emit-del');
-            })
-            .catch((err) => {
-                alert(err.data.message)
-            });
-        },
-    },
-    mounted(){
-        // bootstrap modal 建立
-        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
-            backdrop: 'static',
-            keyboard: false,
-
-        });
-        
-    }
-}
 
 const app = createApp({
 
@@ -130,7 +60,7 @@ const app = createApp({
                 .then((res) => {
                     //驗證成功
                     //取得產品資料
-                    this.getProducts();
+                    this.updateProducts();
                     
                 })
                 .catch((err) => {
@@ -139,7 +69,7 @@ const app = createApp({
                     window.location = 'login.html';
                 });
         },
-        getProducts(page = 1){
+        updateProducts(page = 1){
             //取得所有產品
             const url = `${this.apiUrl}/api/${this.api_path}/admin/products?page=${page}`;
             axios.get(url)
@@ -152,31 +82,6 @@ const app = createApp({
                     alert(err.data.message);
                 });
         },
-        updateProduct() {
-            //新增產品 api 資訊
-            let url = `${this.apiUrl}/api/${this.api_path}/admin/product`;
-            let http = 'post';
-
-            // isNew = false 代表為點擊 編輯產品
-            if (!this.isNew){
-                url = `${this.apiUrl}/api/${this.api_path}/admin/product/${this.productItem.id}`;
-                http = 'put'
-            }
-
-            axios[http](url, {data: this.productItem})
-            .then((res) => {
-                alert(res.data.message);
-                //關閉 modal
-                productModal.hide();
-                //更新 產品列表
-                this.getProducts();
-            })
-            .catch((err) => {
-                alert(err.data.message);
-            })
-        },
-        
-        
         // modal 控制
         openModal(isNew, item){
             // 新增產品
@@ -204,7 +109,6 @@ const app = createApp({
         
     },
     mounted(){
-       
         this.checkAdmin();
     }
 });
