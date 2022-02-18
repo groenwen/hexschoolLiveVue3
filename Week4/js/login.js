c0b47d681bf7fabab929e1bde41a00159656098f
yawen <S2004001@samebest.com.tw>
2022-02-18 17:29:41 +0800

    new Week4

---
+++ Week4/js/login.js
@@ -0,0 +1,37 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+
+    data(){
+        return {
+            user: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+    methods:{
+        login(){
+            const url = `https://vue3-course-api.hexschool.io/v2/admin/signin`;
+            axios.post(url, this.user)
+            .then((res) => {
+                //登入成功
+                // 取得 登入資料 並存入 cookie
+                const {token, expired } = res.data;
+                document.cookie = `groenToken=${token};expires=${new Date(expired)};path=/`;
+
+                // 登入成功 轉至 產品頁面
+                window.location = `index.html`;
+            })
+            .catch((err) => {
+                //登入失敗
+                //顯示 錯誤訊息
+                alert(err.data.message)
+            })
+        }
+
+    },
+    mounted(){
+
+    }
+}).mount('#app');
--- Week4/js/products.js
+++ Week4/js/products.js
@@ -7,12 +7,88 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 // 產品頁面 - 編輯產品 (put /v2/api/groen/admin/product/{id})
 // 產品頁面 - 刪除產品  (delete /v2/api/groen/admin/product/{id})
 
+// 建立 元件 使用區域註冊
+
 
 let productModal = null;
 let delProductModal = null;
 
 
-createApp({
+const pagination = {
+    template: '#pagination',
+    props: ['pages'],
+    methods:{
+        emitPages(item){
+            this.$emit('emit-pages', item)
+        }
+    }
+}
+const productModalTemp = {
+    template: '#productModalTemp',
+    props: ['isNew', 'productItem'],
+    methods: {
+        emitUpdateProduct(){
+            this.$emit('emit-update-product');
+        },
+        // 多圖-新增圖片
+        createImages() {
+            this.productItem.imagesUrl = [];
+            this.productItem.imagesUrl.push('');
+        },
+        //多圖-刪除 單一圖片
+        delImage(key) {
+            this.productItem.imagesUrl.splice(key, 1);
+        },
+    },
+    mounted(){
+        // bootstrap modal 建立
+        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+            backdrop: 'static',
+            keyboard: false,
+            
+        });
+    }
+}
+
+const delProductModalTemp = {
+    data(){
+        return {
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            api_path: 'groen'
+        }
+    },
+    template: '#delProductModalTemp',
+    props: ['isNew', 'productItem'],
+    methods: {
+        // 刪除產品
+        delProduct(){
+            const url = `${this.apiUrl}/api/${this.api_path}/admin/product/${this.productItem.id}`;
+
+            axios.delete(url, this.productItem.id)
+            .then((res) => {
+                alert(res.data.message);
+                //關閉 modal
+                delProductModal.hide();
+                //更新 產品列表
+                this.$emit('emit-del');
+            })
+            .catch((err) => {
+                alert(err.data.message)
+            });
+        },
+    },
+    mounted(){
+        // bootstrap modal 建立
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            backdrop: 'static',
+            keyboard: false,
+
+        });
+        
+    }
+}
+
+const app = createApp({
 
     data(){
         return {
@@ -22,6 +98,9 @@ createApp({
             //所有產品資訊
             products: [],
 
+            // 頁面資訊
+            pagination: {},
+
             // 共用 modal 判斷點擊 新增/編輯
             isNew: false,
 
@@ -30,6 +109,12 @@ createApp({
                 imagesUrl: [],
             }
         }
+    },
+    components: {
+        pagination,
+        productModalTemp,
+        delProductModalTemp
+        
     },
     methods:{
         checkAdmin(){
@@ -46,6 +131,7 @@ createApp({
                     //驗證成功
                     //取得產品資料
                     this.getProducts();
+                    
                 })
                 .catch((err) => {
                     //驗證錯誤 回到登入頁面
@@ -53,13 +139,14 @@ createApp({
                     window.location = 'login.html';
                 });
         },
-        getProducts(){
+        getProducts(page = 1){
             //取得所有產品
-            const url = `${this.apiUrl}/api/${this.api_path}/admin/products/all`;
+            const url = `${this.apiUrl}/api/${this.api_path}/admin/products?page=${page}`;
             axios.get(url)
                 .then((res) => {
                     // 取得所有產品資料 存入 products
                     this.products = res.data.products;
+                    this.pagination = res.data.pagination;
                 })
                 .catch((err) => {
                     alert(err.data.message);
@@ -88,31 +175,8 @@ createApp({
                 alert(err.data.message);
             })
         },
-        // 刪除產品
-        delProduct(){
-            const url = `${this.apiUrl}/api/${this.api_path}/admin/product/${this.productItem.id}`;
-
-            axios.delete(url, this.productItem.id)
-            .then((res) => {
-                alert(res.data.message);
-                //關閉 modal
-                delProductModal.hide();
-                //更新 產品列表
-                this.getProducts();
-            })
-            .catch((err) => {
-                alert(err.data.message)
-            });
-        },
-        // 多圖-新增圖片
-        createImages() {
-            this.productItem.imagesUrl = [];
-            this.productItem.imagesUrl.push('');
-        },
-        //多圖-刪除 單一圖片
-        delImage(key) {
-            this.productItem.imagesUrl.splice(key, 1);
-        },
+        
+        
         // modal 控制
         openModal(isNew, item){
             // 新增產品
@@ -140,19 +204,9 @@ createApp({
         
     },
     mounted(){
-        // bootstrap modal 建立
-        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
-            backdrop: 'static',
-            keyboard: false,
-            
-        });
-        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
-            backdrop: 'static',
-            keyboard: false,
-
-        });
-
-        
+       
         this.checkAdmin();
     }
-}).mount('#app');
+});
+
+app.mount('#app');
