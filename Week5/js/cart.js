daa75605d3d232efea47a12df2d8e3bb2108892b
yawen <S2004001@samebest.com.tw>
2022-03-02 18:06:20 +0800

    edit Week5

--- Week5/js/cart.js
+++ Week5/js/cart.js
@@ -1,8 +1,48 @@
 // 產品列出來
-// 元件 - 查看 產品 modal
+// 元件 - 查看 產品 modal 單一產品
 // 元件 - 做出分頁 區域註冊
 // 加產品到購物車
 // 驗證
+
+
+//先建元件
+//資料橋梁嫁接
+const pagination = {
+  data(){
+    return {
+
+    }
+  },
+  props: ['pagination'],
+  template: '#paginationTemp',
+  methods: {
+    emitPages(item){
+      this.$emit('emit-pages', item)
+    }
+  }
+
+
+}
+const productModal = {
+  data(){
+    return {
+      thisModal: '',
+    }
+  },
+  props: ['productItem'],
+  template: '#productModalTemp',
+  methods: {
+    innerOpenModal(){
+      this.thisModal.show();
+    },
+    
+  },
+  mounted(){
+    this.thisModal = new bootstrap.Modal(this.$refs.productModal, {
+      keybord: false
+    })
+  }
+}
 
 
 
@@ -11,23 +51,67 @@ const app = Vue.createApp({
     return {
       api_url: "https://vue3-course-api.hexschool.io/v2",
       api_path: "groen",
-      products: []
+      //所有產品
+      products: [],
+
+      //頁面資訊
+      pagination: {},
+
+      //單一產品
+      productItem: {},
+
+      //購物車列表
+      carts: []
     };
   },
+  components: {
+    productModal, pagination
+  },
   methods: {
-    getProducts() {
-      let url = `${this.api_url}/api/${this.api_path}/products`;
+    updateProducts(page = 1) {
+      let url = `${this.api_url}/api/${this.api_path}/products?page=${page}`;
       axios.get(url)
       .then((res) => {
         this.products = res.data.products;
+        this.pagination = res.data.pagination;
       })
       .catch((err) => {
         alert(err.data.message);
       });
     },
+    openModal(item){
+      this.$refs.callModal.innerOpenModal();
+      this.productItem = item;
+    },
+    addToCart(id){
+      let url = `${this.api_url}/api/${this.api_path}/cart`;
+
+      axios.post(url, {data:{'product_id': id, 'qty': 1 }})
+      .then((res) => {
+        alert(res.data.message);
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+      
+    },
+    updateCart(){
+      let url = `${this.api_url}/api/${this.api_path}/cart`;
+
+      axios.get(url)
+      .then((res) => {
+        this.carts = res.data.carts;
+        console.log(this.carts, hello);
+      })
+      .catch((err) => {
+        alert(err.data);
+      });
+    }
+
   },
   mounted() {
-    this.getProducts();
+    this.updateProducts();
+    this.updateCart();
   },
 });
 
